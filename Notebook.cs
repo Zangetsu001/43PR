450850307a2cca92310dee1619d785d4c07fa649
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-05-22 10:46:35 +0300

    Add files via upload

---
+++ Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public struct Notebook
+    {
+        public string Model;
+        public string Manufacturer;
+        public double Price;
+        public Notebook(string model, string manufacturer, double price)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Модель: " + Model);
+            Console.WriteLine("Производитель: " + Manufacturer);
+            Console.WriteLine("Цена: " + Price);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите модель ноутбука: ");
+            string model = Console.ReadLine();
+            Console.Write("Введите производителя ноутбука: ");
+            string manufacturer = Console.ReadLine();
+
+            double price;
+            while (true)
+            {
+                Console.Write("Введите цену ноутбука: ");
+                if (double.TryParse(Console.ReadLine(), out price) && price > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод. Пожалуйста, введите корректную цену.");
+                }
+            }
+            Notebook notebook = new Notebook(model, manufacturer, price);
+            notebook.DisplayInfo();
+            Console.Read();
+
+        }
+    }
+}
