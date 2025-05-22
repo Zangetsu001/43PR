6b63e348f3c4129c4f591d777c7113ca97ffea2e
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-05-22 10:46:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            Console.WriteLine("Изменено");
+        }
+        static void StructTaker(MyStruct myStruct)
+        {
+            Console.WriteLine("Изменено");
+        }
+        static void Main(string[] args)
+        {
+            MyClass classInstance = new MyClass();
+            classInstance.change = "Не изменено";
+            MyStruct structInstance = new MyStruct();
+            structInstance.change = "Не изменено";
+            ClassTaker(classInstance);
+            StructTaker(structInstance);
+            Console.WriteLine($"Класс: " +  classInstance.change);
+            Console.WriteLine($"Стурктура: " +  structInstance.change);
+            Console.Read();
+        }
+    }
+}
