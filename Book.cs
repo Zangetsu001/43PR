ebb9589d5cb626dbd234236a242578d35154799f
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-05-22 10:54:28 +0300

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp32
 {
-    class Book
+    public struct Book
     {
+        public string Title;          
+        public string Author;          
+        public int YearOfPublication;  
+        public int Pages;              
+
+        
+        public Book(string title, string author, int yearOfPublication, int pages)
+        {
+            Title = title;
+            Author = author;
+            YearOfPublication = yearOfPublication;
+            Pages = pages;
+        }
+
+   
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Название: " + Title);
+            Console.WriteLine($"Автор: " + Author);
+            Console.WriteLine($"Год издания: " + YearOfPublication);
+            Console.WriteLine($"Количество страниц: " + Pages);
+            Console.WriteLine(); 
+        }
     }
 }
